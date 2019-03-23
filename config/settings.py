fd96e5d6e38a18ac34ee12b53b6cf51e9949652f
Herman Singh <kartstig@gmail.com>
2019-03-23 14:45:06 -0400

    typo

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staic/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = "/login"
