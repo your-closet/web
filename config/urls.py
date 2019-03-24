42ce40f33bf3e52c2b07a98b2c82de3b157247db
ducoterra <ducoterra@gmail.com>
2019-03-24 16:10:42 -0400

    test media settings change

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include("public.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
