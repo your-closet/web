64e21dd7733f3d97f0afbd0f9139cf156b91dcc5
ducoterra <ducoterra@gmail.com>
2019-03-24 16:13:51 -0400

    fuck media

--- config/settings.py
+++ config/settings.py
@@ -190,5 +190,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = "/login"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
